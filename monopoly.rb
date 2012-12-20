824bfd1ef3d66a5156bbebe33b41a192b259c7e9
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2012-12-20 22:58:32 +0100

    let players give moves

--- monopoly.rb
+++ monopoly.rb
@@ -2,23 +2,30 @@ module Monopoly
   class Game
     def initialize
       @players = []
+      @player_made_moves = {}
+      @move_requests = []
       @player_moves = {}
     end
 
     def join(player)
       @players << player
+      @player_made_moves[player] = []
       @player_moves[player] = []
+      3.times { @player_moves[player] << Move.new }
     end
 
     def play(player)
       raise NoMoreMoves if no_more_moves?(player)
-      @player_moves[player] << Move.new
+      @player_made_moves[player] << @player_moves[player].shift
     end
 
     def no_more_moves?(player)
-      @player_moves[player].length >= 3
+      @player_moves[player].length == 0
     end
 
+    def give_move(player, friend)
+      @player_moves[friend] << Move.new
+    end
   end
   class NoMoreMoves < Exception
   end
@@ -27,5 +34,10 @@ module Monopoly
   end
 
   class Move
+  end
+
+  class MoveRequest
+    def initialize(from_player, to_player)
+    end
   end
 end
