8a552832d9969d0f85c6055b48fc190e014433a7
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2012-12-20 22:24:26 +0100

    moves are limited to 3

--- monopoly_test.rb
+++ monopoly_test.rb
@@ -1,14 +1,68 @@
 require 'test/unit'
+  
 class MonopolyTest  < Test::Unit::TestCase
-  def test_fresh_game
-    monopoly = MonopolyGame.new
-    assert monopoly.players.length == 0
+  def new_monopoly_game
+    Monopoly::Game.new
+  end
+
+  def new_player
+    Monopoly::Player.new
+  end
+
+  def test_game_allows_many_players
+    monopoly = new_monopoly_game
+    andrzej  = new_player
+    nthx     = new_player
+
+    monopoly.join(andrzej)
+    monopoly.join(nthx)
+    monopoly.play(andrzej)
+    monopoly.play(nthx)
+    monopoly.play(nthx)
+  end
+
+  def test_game_limits_moves_to_3
+    monopoly = new_monopoly_game
+    andrzej  = new_player
+    monopoly.join(andrzej)
+
+    monopoly.play(andrzej)
+    monopoly.play(andrzej)
+    monopoly.play(andrzej)
+    assert_raises Monopoly::NoMoreMoves do
+      monopoly.play(andrzej)
+    end
   end
 end
 
-class MonopolyGame
-  attr_reader :players
-  def initialize
-    @players = []
+module Monopoly
+  class Game
+    def initialize
+      @players = []
+      @player_moves = {}
+    end
+
+    def join(player)
+      @players << player
+      @player_moves[player] = []
+    end
+
+    def play(player)
+      raise NoMoreMoves if no_more_moves?(player)
+      @player_moves[player] << Move.new
+    end
+
+    def no_more_moves?(player)
+      @player_moves[player].length >= 3
+    end
+
+  end
+  class NoMoreMoves < Exception
+  end
+  class Player
+
+  end
+
+  class Move
   end
 end
