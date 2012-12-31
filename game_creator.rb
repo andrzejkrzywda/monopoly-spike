f7365c0cca85dc362b93f9705f83175a5ff49610
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2012-12-31 01:12:03 +0100

    moved all the policies out of the game use-case. it's now injected in the game_creator using the before/after advices (AOP).

--- game_creator.rb
+++ game_creator.rb
@@ -20,42 +20,46 @@ include Aquarium::Aspects
 module Monopoly
   class GameCreator
     def create_default_monopoly_game(players = [], board = Board::Board.new(16))
-      game = MonopolyPlayGameUseCase.new(board, 
-                                          default_join_game_rules,
-                                          default_before_make_move_rules,
-                                          default_after_make_move_policies, 
-                                          default_buying_policies)
+      game = MonopolyPlayGameUseCase.new(board)
+      
+      default_join_game_rules(game, board)
+      default_before_make_move_rules(game)
+      default_after_make_move_policies(game, board)
+      default_buying_policies(game, board)
+      
+
       players.each {|player| game.join(player)}
-      Aspect.new :before, methods: [:join], for_objects: [game] do |jp, game, player|
-        AddInitialNumberOfLifes.new.apply(nil, player)
-      end
+
       return game
     end
 
-    def default_join_game_rules
-      [
-        #AddInitialNumberOfLifes.new(3),
-        PutPlayerOnBoardInitialField.new
-      ]
+    def default_join_game_rules(game, board)
+      Aspect.new :before, methods: [:join], for_objects: [game] do |jp, game, player|
+        AddInitialNumberOfLifes.new.apply(board, player)
+        PutPlayerOnBoardInitialField.new.apply(board, player)
+      end
     end
 
-    def default_before_make_move_rules
-      [MoveCostsLife.new]
+    def default_before_make_move_rules(game)
+      Aspect.new :before, methods: [:make_move], for_objects: [game] do |jp, game, player|
+        MoveCostsLife.new.apply(player)
+      end
     end
 
-    def default_after_make_move_policies
-      [
-        BonusForMeetingFriendsAtTheSameField.new,
-        GiveBonusPointsToFriendsWhenVisitingTheirProperty.new
-      ]
+    def default_after_make_move_policies(game, board)
+      Aspect.new :after, methods: [:make_move], for_objects: [game] do |jp, game, player|
+        BonusForMeetingFriendsAtTheSameField.new.apply(board, player)
+        GiveBonusPointsToFriendsWhenVisitingTheirProperty.new.apply(board, player)
+      end
     end
 
-    def default_buying_policies
-      [
-        NothingToBuyOnThisFieldPolicy.new,
-        AlreadyBoughtPolicy.new,
-        CantAffordPolicy.new
-      ]
+    def default_buying_policies(game, board)
+      Aspect.new :before, methods: [:buy], for_objects: [game] do |jp, game, player|
+        field = board.player_field(player)
+        NothingToBuyOnThisFieldPolicy.new.apply(player, field)
+        AlreadyBoughtPolicy.new.apply(player, field)
+        CantAffordPolicy.new.apply(player, field)
+      end
     end
   end
   
--- monopoly.rb
+++ monopoly.rb
@@ -1,35 +1,21 @@
 module Monopoly 
 
   class MonopolyPlayGameUseCase
-    def initialize(board, join_game_rules=[], 
-                   before_make_move_rules=[],
-                   after_make_move_policies=[], 
-                   buying_policies=[])
+    def initialize(board)
       @dice_roller = DiceRoller.new
       @board = board
-      @join_game_rules = join_game_rules
-      @before_make_move_rules = before_make_move_rules
-      @after_make_move_policies = after_make_move_policies
-      @buying_policies = buying_policies
     end
 
     def join(player)
-      @join_game_rules.each {|rule| rule.apply(@board, player)}
     end
 
     def make_move(player)
-      @before_make_move_rules.each {|rule| rule.apply(player)}
-      
       dice_roll = @dice_roller.roll_2
       @board.move_player_by(player, dice_roll)
-      
-      @after_make_move_policies.each {|p| p.apply(@board, player)}
     end
 
     def buy(player)
       current_field = @board.player_field(player)
-      @buying_policies.each {|policy| policy.apply(player, current_field)}
-
       property = current_field.property
       player.pay(property.points_price)
       player.add_property(property)
@@ -39,9 +25,5 @@ module Monopoly
     def give_life(from_player, to_player)
       to_player.add_life
     end
-
-
   end
-
-
 end
