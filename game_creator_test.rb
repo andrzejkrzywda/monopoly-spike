ef3d6b30336b2863e8877be00d83f354b13da31b
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2013-01-05 11:25:20 +0100

    make the game test a little bit more acceptancish

--- game_creator_test.rb
+++ game_creator_test.rb
@@ -27,14 +27,16 @@ class GameCreatorTest  < Test::Unit::TestCase
   def test_buying_not_possible
     board   = Board.new(16)
     andrzej = Player.new    
+    cheater_dice_roller = CheaterDiceRoller.new
     nike_shop = Property.new("Nike shop", 100, 42)
     board.put_property_on(1, nike_shop)
-    board.set_player_position(andrzej, 0)
-    monopoly = GameCreator.new.create_default_monopoly_game(board)
+
+    monopoly = GameCreator.new.create_default_monopoly_game(board, cheater_dice_roller)
     monopoly.join(andrzej)
     assert_raise(NothingToBuyOnThisField) { monopoly.buy(andrzej) }
 
-    board.move_player_by(andrzej, 1)
+    cheater_dice_roller.next_roll_will_be(1)
+    monopoly.make_move(andrzej)
     assert_raise(CantAfford) { monopoly.buy(andrzej) }
 
     andrzej.add_property(nike_shop)
@@ -46,11 +48,13 @@ class GameCreatorTest  < Test::Unit::TestCase
   def test_buying
     board   = Board.new(16)
     andrzej = Player.new    
+    cheater_dice_roller = CheaterDiceRoller.new
     nike_shop = Property.new("Nike shop", 100, 0)
     board.put_property_on(1, nike_shop)
-    monopoly = GameCreator.new.create_default_monopoly_game(board)
+    monopoly = GameCreator.new.create_default_monopoly_game(board, cheater_dice_roller)
     monopoly.join(andrzej)
-    board.move_player_by(andrzej, 1)
+    cheater_dice_roller.next_roll_will_be(1)
+    monopoly.make_move(andrzej)
     andrzej.add_points(120)
     
     monopoly.buy(andrzej)
